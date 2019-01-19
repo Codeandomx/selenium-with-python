7d7a96cac983437edbcdcac09d05fd5d402740d1
Paulo andrade <source.compu@gmail.com>
2019-01-19 09:15:20 -0600

    init commit

---
+++ firefox_base.py
@@ -0,0 +1,33 @@
+# importamos la libreria
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+
+def getFirefox():
+    # For linux
+    # from pyvirtualdisplay import Display
+    # Creamos un entorno virtual
+    # display = Display(visible=0,size=(1024,768))
+    # display.start()
+
+    # options = Options()
+    # options.headless = True
+
+    # Creamos una instancia
+    # driver = webdriver.Firefox(options=options)
+    driver = webdriver.Firefox()
+    # driver = webdriver.Firefox(executable_path='/usr/local/bin/geckodriver')
+
+    # Vamos al sitio web
+    driver.get("http://www.goodstartbooks.com")
+
+    driver.implicitly_wait(5) # sleep for 10 seconds
+
+    print("Page Title is : %s" %driver.title)
+
+    # cerramos el driver
+    driver.quit()
+
+
+if __name__ == "__main__":
+    getFirefox()
